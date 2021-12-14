24d5db55e9260fa5ca89211f8817e7bd9cc6e17d
Christopher Luey <christopherluey@gmail.com>
2021-12-14 03:21:18 -0800

    easy problem

---
+++ src/LCS412.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int i = 1;
+        List<String> list = new ArrayList<>();
+        while (i <= n) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(Integer.toString(i));
+            }
+            i++;
+        }
+        return list;
+    }
+}
