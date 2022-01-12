1380ee61c08a90e15e4680a523c33de34a35d970
Christopher Luey <christopherluey@gmail.com>
2022-01-12 01:03:58 -0800

    day 11 practice

---
+++ src/LCS303.cpp
@@ -0,0 +1,22 @@
+class NumArray {
+public:
+    
+    vector<int> arr;
+    
+    NumArray(vector<int>& nums) {
+        arr.push_back(0);
+        for (int i = 0; i < nums.size(); i++) {
+            arr.push_back(arr[i] + nums[i]);
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return arr[right + 1] - arr[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
