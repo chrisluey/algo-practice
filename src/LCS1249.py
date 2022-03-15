6ce7a13407bda65feab283c1397e84e61fb0ec88
Christopher Luey <christopherluey@gmail.com>
2022-03-15 02:03:52 -0700

    changed sol for pref

--- src/LCS1249.py
+++ src/LCS1249.py
@@ -1,17 +1,15 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         this_list = []
-        j = 0
         list_s = list(s)
         for i in range(len(list_s)):
             if list_s[i] == ")":
-                if len(this_list) > 0 and j < len(this_list) and this_list[j] < i:
-                    j += 1
+                if len(this_list) > 0:
+                    this_list.pop(0)
                 else:
                     list_s[i] = ""
             elif list_s[i] == "(":
                 this_list.append(i)
-        for k in range(j,len(this_list)):
-            list_s[this_list[k]] = ""
-        
+        for j in this_list:
+            list_s[j] = ""
         return "".join(list_s)
